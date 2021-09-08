67a819cda8666d692a33ee9396a8e011e5d018f9
israel <israel.pereira@dcx.ufpb.br>
2021-09-08 09:15:53 -0300

    arquivos📦

---
+++ src/main/java/com/crud/swegger/SweggerApplication.java
@@ -0,0 +1,13 @@
+package com.crud.swegger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/swegger/SweggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.swegger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
